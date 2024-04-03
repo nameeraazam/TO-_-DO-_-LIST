080e46742e5b2946e6cd0d66a60b55ab4da3ffaf
Nameera Azam <163463330+nameeraazam@users.noreply.github.com>
2024-04-03 16:10:07 +0500

    Add files via upload
    My TO_DO_LIST project completed

---
+++ main.js
@@ -0,0 +1,31 @@
+#! /na74p/usr/bin/env node
+//TODO APP IN TYPESCRIPT BY NAMEERA AZAM
+import inquirer from "inquirer";
+//user available
+let todo = [];
+while (true) {
+    let input = await inquirer.prompt([
+        {
+            name: "Todoitem",
+            type: "input",
+            message: "add todo item and listing",
+        },
+        {
+            name: "addMore",
+            type: "list",
+            choices: ["yes", "no"],
+        },
+    ]);
+    //output of the program
+    const { Todoitem, addMore } = input;
+    todo.push(Todoitem);
+    if (addMore == "no") {
+        console.log("Todolist:");
+        //add more item
+        for (let i = 0; i < todo.length; i++) {
+            console.log(todo[i]);
+        }
+        //exit loop
+        break;
+    }
+}
